8e6fb55e352d7ae98f10d5f9044545f812724053
Nachiket Patil <nacpatil@latticerobot.com>
2025-02-21 12:13:20 -0500

    cmake added and built on windows

---
+++ PerformanceTest.cpp
@@ -0,0 +1,51 @@
+#include "PerformanceTest.h"
+#include <random>
+#include <omp.h>
+#include <chrono>
+
+void PerformanceTest::runPerformance() {
+    size_t numRows = 30000000;  // Number of rows
+
+    std::cout << "\n\n=============\nRunning performance test on large data with vector size : " << numRows << " x 7 columns" << "\n";
+    std::cout << "Generating permutated/mismashed large size table.\n";
+
+    Table randomIntTable = {
+        AnyColumn(generateSequentialIntVector(numRows,2)),
+        AnyColumn(generateSequentialIntVector(numRows,1)),
+        AnyColumn(generateSequentialIntVector(numRows,3)),
+        AnyColumn(generateSequentialIntVector(numRows,4)),
+        AnyColumn(generateSequentialIntVector(numRows,5)),
+        AnyColumn(generateSequentialIntVector(numRows,6)),
+        AnyColumn(generateSequentialIntVector(numRows,7))
+    };
+    std::cout << "Done generating input table.\n";
+
+    auto sort_start_time = std::chrono::high_resolution_clock::now();
+
+    randomIntTable.sort();
+
+    auto sort_end_time = std::chrono::high_resolution_clock::now();
+
+    std::chrono::duration<double> sort_duration = sort_end_time - sort_start_time;
+
+    std::cout << "Done generating random vector \n";
+    std::cout << "Sorting time: " << sort_duration.count() << " seconds\n";
+
+}
+
+
+std::vector<int> PerformanceTest::generateSequentialIntVector(size_t size, int thisNumber) {
+    std::vector<int> values(size);
+
+#pragma omp parallel for
+    for (size_t i = 0; i < size; ++i) {
+        values[i] = (thisNumber + i - 1) % 9000 + 1;
+    }
+    return values;
+}
+
+
+
+
+
+
---
+++ PerformanceTest.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Table.h"
+
+class PerformanceTest 
+{
+	public: 
+		void runPerformance();
+		std::vector<int> generateSequentialIntVector(size_t size, int thisNumber);
+};
+
+
+
+
+
+
